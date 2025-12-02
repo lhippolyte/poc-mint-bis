779625545ebc7057aaeaf16e613d5964dd2367c7
hippo <hippolyte.lacassagne@gmail.com>
2025-12-02 11:14:42 +0100

    update

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
+
+const port = process.env.PORT || 4000 
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
